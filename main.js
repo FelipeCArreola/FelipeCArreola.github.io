1e25f31a23d0d9d2d667054861447e1a6b02d2b7
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-05 18:14:50 -0800

    Update main.js
    Giving source credit of JS being utilized that supports my Drop Down button.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-/* Drop Down Function-Nav Bar */
+/* Drop Down Function-Nav Bar -----**Source**:https://www.w3schools.com/css/css_dropdowns.asp" */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it -----**Source**:https://www.w3schools.com/css/css_dropdowns.asp" //
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
